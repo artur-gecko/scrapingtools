91f07c13a6a7a7bfcf99252be4d5b86648e47d7a
Artur Majewski <artur.majewski@gecko-lab.com>
2025-04-01 12:07:54 +0200

    Add semi-automatic Clatch scrapper

--- ruby_scrapper.rb
+++ ruby_scrapper.rb
@@ -6,23 +6,29 @@ options = Selenium::WebDriver::Chrome::Options.new
 #options.add_argument("--headless") # comment out in development
 # create a driver instance to control Chrome
 # with the specified options
+
+File.open("clutchsyf.txt", "r") do |file_handle|
+file_handle.each_line do |line|
 driver = Selenium::WebDriver.for :chrome, options: options
 
+
 # connect to the target page
-driver.navigate.to "https://clutch.co/profile/ideamotive"
+driver.navigate.to line
 
 # extract the HTML of the target page
 # and print it
 html = driver.page_source
 #puts html
-loc_thematch = html.match(/locality.*/)
-puts loc_thematch
+loc_thematch = html.match(/locality.*/)[0][19..-3]
+#puts loc_thematch
 
-url_thematch = html.match(/.*sameAs.*/)[0].split("content=\"")[1]
-puts url_thematch
+url_thematch = html.match(/.*sameAs.*/)[0].split("content=\"")[1][0...-2]
+#puts url_thematch
 
-name_match = html.match(/og:title\" content=\"[A-Za-z\-0-9]*/)[0].split("content=\"")[1]
+name_match = html.match(/og:title\" content=\"[A-Za-z\-0-9].*"/)[0][19..-2]
 
-puts name_match
+puts name_match+"\t"+loc_thematch+"\t"+url_thematch
 # close the browser and release its resources
 driver.quit
+end
+end
